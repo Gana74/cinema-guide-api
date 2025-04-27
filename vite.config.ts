eb1279dcf9a38b64016d9fec840cb1d424ef1870
Dmitrii Appel <yamixam@gmail.com>
2025-04-27 16:35:01 +0500

    chore: перемещение проекта в отдельный репозиторий

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,10 @@
-// import { defineConfig } from "vite";
-// import vue from "@vitejs/plugin-vue";
-
-// export default defineConfig({
-//   plugins: [vue()],
-//   resolve: {
-//     alias: {
-//       "@": "/src",
-//     },
-//   },
-// });
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   plugins: [vue()],
+  base: "/cinema-guide-api/", 
   build: {
-    target: "esnext", // Убедитесь, что target соответствует вашей версии ECMAScript
+    target: "esnext",
   },
 });
